061e4f324f1613918d908265bda7daa4e4bc4579
Steve Cavanagh <s.cavanagh@att.net>
2015-01-12 21:28:33 -0600

    fix(demo): Register controllers in module, not as globals 1.3 compat

--- misc/demo/assets/app.js
+++ misc/demo/assets/app.js
@@ -27,6 +27,173 @@ angular.module('ui.bootstrap.demo', ['ui.bootstrap', 'plunker', 'ngTouch'], func
     };
   }]
 )
+  .controller('SelectModulesCtrl', ['$scope', '$modalInstance', 'modules', 'buildFilesService', function ($scope, $modalInstance, modules, buildFilesService) {
+    $scope.selectedModules = [];
+    $scope.modules = modules;
+
+    $scope.selectedChanged = function (module, selected) {
+      if (selected) {
+        $scope.selectedModules.push(module);
+      } else {
+        $scope.selectedModules.splice($scope.selectedModules.indexOf(module), 1);
+      }
+    };
+
+    $scope.downloadBuild = function () {
+      $modalInstance.close($scope.selectedModules);
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss();
+    };
+
+    $scope.isOldBrowser = function () {
+      return isOldBrowser;
+    };
+
+    $scope.build = function (selectedModules, version) {
+      /* global JSZip, saveAs */
+      var moduleMap, rawFiles;
+
+      buildFilesService.get().then(function (buildFiles) {
+        moduleMap = buildFiles.moduleMap;
+        rawFiles = buildFiles.rawFiles;
+
+        generateBuild();
+      });
+
+      function generateBuild() {
+        var srcModuleNames = selectedModules
+          .map(function (module) {
+            return moduleMap[module];
+          })
+          .reduce(function (toBuild, module) {
+            addIfNotExists(toBuild, module.name);
+
+            module.dependencies.forEach(function (depName) {
+              addIfNotExists(toBuild, depName);
+            });
+            return toBuild;
+          }, []);
+
+        var srcModules = srcModuleNames
+          .map(function (moduleName) {
+            return moduleMap[moduleName];
+          });
+
+        var srcModuleFullNames = srcModules
+          .map(function (module) {
+            return module.moduleName;
+          });
+
+        var srcJsContent = srcModules
+            .reduce(function (buildFiles, module) {
+              return buildFiles.concat(module.srcFiles);
+            }, [])
+            .map(getFileContent)
+            .join('\n')
+          ;
+
+        var jsFile = createNoTplFile(srcModuleFullNames, srcJsContent);
+
+        var tplModuleNames = srcModules
+          .reduce(function (tplModuleNames, module) {
+            return tplModuleNames.concat(module.tplModules);
+          }, []);
+
+        var tplJsContent = srcModules
+            .reduce(function (buildFiles, module) {
+              return buildFiles.concat(module.tpljsFiles);
+            }, [])
+            .map(getFileContent)
+            .join('\n')
+          ;
+
+        var jsTplFile = createWithTplFile(srcModuleFullNames, srcJsContent, tplModuleNames, tplJsContent);
+
+        var zip = new JSZip();
+        zip.file('ui-bootstrap-custom-' + version + '.js', rawFiles.banner + jsFile);
+        zip.file('ui-bootstrap-custom-' + version + '.min.js', rawFiles.banner + uglify(jsFile));
+        zip.file('ui-bootstrap-custom-tpls-' + version + '.js', rawFiles.banner + jsTplFile);
+        zip.file('ui-bootstrap-custom-tpls-' + version + '.min.js', rawFiles.banner + uglify(jsTplFile));
+
+        saveAs(zip.generate({type: 'blob'}), 'ui-bootstrap-custom-build.zip');
+      }
+
+      function createNoTplFile(srcModuleNames, srcJsContent) {
+        return 'angular.module("ui.bootstrap", [' + srcModuleNames.join(',') + ']);\n' +
+          srcJsContent;
+      }
+
+      function createWithTplFile(srcModuleNames, srcJsContent, tplModuleNames, tplJsContent) {
+        var depModuleNames = srcModuleNames.slice();
+        depModuleNames.unshift('"ui.bootstrap.tpls"');
+
+        return 'angular.module("ui.bootstrap", [' + depModuleNames.join(',') + ']);\n' +
+          'angular.module("ui.bootstrap.tpls", [' + tplModuleNames.join(',') + ']);\n' +
+          srcJsContent + '\n' + tplJsContent;
+
+      }
+
+      function addIfNotExists(array, element) {
+        if (array.indexOf(element) == -1) {
+          array.push(element);
+        }
+      }
+
+      function getFileContent(fileName) {
+        return rawFiles.files[fileName];
+      }
+
+      function uglify(js) {
+        /* global UglifyJS */
+
+        var ast = UglifyJS.parse(js);
+        ast.figure_out_scope();
+
+        var compressor = UglifyJS.Compressor();
+        var compressedAst = ast.transform(compressor);
+
+        compressedAst.figure_out_scope();
+        compressedAst.compute_char_frequency();
+        compressedAst.mangle_names();
+
+        var stream = UglifyJS.OutputStream();
+        compressedAst.print(stream);
+
+        return stream.toString();
+      }
+    };
+  }]
+)
+  .controller('DownloadCtrl', ['$scope', '$modalInstance', function ($scope, $modalInstance) {
+
+    $scope.options = {
+      minified: true,
+      tpls: true
+    };
+
+    $scope.download = function (version) {
+      var options = $scope.options;
+
+      var downloadUrl = ['ui-bootstrap-'];
+      if (options.tpls) {
+        downloadUrl.push('tpls-');
+      }
+      downloadUrl.push(version);
+      if (options.minified) {
+        downloadUrl.push('.min');
+      }
+      downloadUrl.push('.js');
+
+      return downloadUrl.join('');
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss();
+    };
+  }]
+)
   .run(['$location', function ($location) {
     //Allows us to navigate to the correct element on initialization
     if ($location.path() !== '' && $location.path() !== '/') {
@@ -45,7 +212,7 @@ angular.module('ui.bootstrap.demo', ['ui.bootstrap', 'plunker', 'ngTouch'], func
       get: function () {
         return $q.all({
           moduleMap: getModuleMap(),
-          rawFiles: getRawFiles(),
+          rawFiles: getRawFiles()
         });
       }
     };
@@ -67,174 +234,6 @@ angular.module('ui.bootstrap.demo', ['ui.bootstrap', 'plunker', 'ngTouch'], func
     }
 
   });
-
-var builderUrl = "http://50.116.42.77:3001";
-
-var SelectModulesCtrl = function ($scope, $modalInstance, modules, buildFilesService) {
-  $scope.selectedModules = [];
-  $scope.modules = modules;
-
-  $scope.selectedChanged = function (module, selected) {
-    if (selected) {
-      $scope.selectedModules.push(module);
-    } else {
-      $scope.selectedModules.splice($scope.selectedModules.indexOf(module), 1);
-    }
-  };
-
-  $scope.downloadBuild = function () {
-    $modalInstance.close($scope.selectedModules);
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss();
-  };
-
-  $scope.isOldBrowser = function () {
-    return isOldBrowser;
-  };
-
-  $scope.build = function (selectedModules, version) {
-    /* global JSZip, saveAs */
-    var moduleMap, rawFiles;
-
-    buildFilesService.get().then(function (buildFiles) {
-      moduleMap = buildFiles.moduleMap;
-      rawFiles = buildFiles.rawFiles;
-
-      generateBuild();
-    });
-
-    function generateBuild() {
-      var srcModuleNames = selectedModules
-        .map(function (module) {
-          return moduleMap[module];
-        })
-        .reduce(function (toBuild, module) {
-          addIfNotExists(toBuild, module.name);
-
-          module.dependencies.forEach(function (depName) {
-            addIfNotExists(toBuild, depName);
-          });
-          return toBuild;
-        }, []);
-
-      var srcModules = srcModuleNames
-        .map(function (moduleName) {
-          return moduleMap[moduleName];
-        });
-
-      var srcModuleFullNames = srcModules
-        .map(function (module) {
-          return module.moduleName;
-        });
-
-      var srcJsContent = srcModules
-          .reduce(function (buildFiles, module) {
-            return buildFiles.concat(module.srcFiles);
-          }, [])
-          .map(getFileContent)
-          .join('\n')
-        ;
-
-      var jsFile = createNoTplFile(srcModuleFullNames, srcJsContent);
-
-      var tplModuleNames = srcModules
-        .reduce(function (tplModuleNames, module) {
-          return tplModuleNames.concat(module.tplModules);
-        }, []);
-
-      var tplJsContent = srcModules
-          .reduce(function (buildFiles, module) {
-            return buildFiles.concat(module.tpljsFiles);
-          }, [])
-          .map(getFileContent)
-          .join('\n')
-        ;
-
-      var jsTplFile = createWithTplFile(srcModuleFullNames, srcJsContent, tplModuleNames, tplJsContent);
-
-      var zip = new JSZip();
-      zip.file('ui-bootstrap-custom-' + version + '.js', rawFiles.banner + jsFile);
-      zip.file('ui-bootstrap-custom-' + version + '.min.js', rawFiles.banner + uglify(jsFile));
-      zip.file('ui-bootstrap-custom-tpls-' + version + '.js', rawFiles.banner + jsTplFile);
-      zip.file('ui-bootstrap-custom-tpls-' + version + '.min.js', rawFiles.banner + uglify(jsTplFile));
-
-      saveAs(zip.generate({type: 'blob'}), 'ui-bootstrap-custom-build.zip');
-    }
-
-    function createNoTplFile(srcModuleNames, srcJsContent) {
-      return 'angular.module("ui.bootstrap", [' + srcModuleNames.join(',') + ']);\n' +
-        srcJsContent;
-    }
-
-    function createWithTplFile(srcModuleNames, srcJsContent, tplModuleNames, tplJsContent) {
-      var depModuleNames = srcModuleNames.slice();
-      depModuleNames.unshift('"ui.bootstrap.tpls"');
-
-      return 'angular.module("ui.bootstrap", [' + depModuleNames.join(',') + ']);\n' +
-        'angular.module("ui.bootstrap.tpls", [' + tplModuleNames.join(',') + ']);\n' +
-        srcJsContent + '\n' + tplJsContent;
-
-    }
-
-    function addIfNotExists(array, element) {
-      if (array.indexOf(element) == -1) {
-        array.push(element);
-      }
-    }
-
-    function getFileContent(fileName) {
-      return rawFiles.files[fileName];
-    }
-
-    function uglify(js) {
-      /* global UglifyJS */
-
-      var ast = UglifyJS.parse(js);
-      ast.figure_out_scope();
-
-      var compressor = UglifyJS.Compressor();
-      var compressedAst = ast.transform(compressor);
-
-      compressedAst.figure_out_scope();
-      compressedAst.compute_char_frequency();
-      compressedAst.mangle_names();
-
-      var stream = UglifyJS.OutputStream();
-      compressedAst.print(stream);
-
-      return stream.toString();
-    }
-  };
-};
-
-var DownloadCtrl = function ($scope, $modalInstance) {
-  $scope.options = {
-    minified: true,
-    tpls: true
-  };
-
-  $scope.download = function (version) {
-    var options = $scope.options;
-
-    var downloadUrl = ['ui-bootstrap-'];
-    if (options.tpls) {
-      downloadUrl.push('tpls-');
-    }
-    downloadUrl.push(version);
-    if (options.minified) {
-      downloadUrl.push('.min');
-    }
-    downloadUrl.push('.js');
-
-    return downloadUrl.join('');
-  };
-
-  $scope.cancel = function () {
-    $modalInstance.dismiss();
-  };
-};
 
 /*
  * The following compatibility check is from:
